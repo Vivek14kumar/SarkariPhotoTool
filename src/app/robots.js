984e9b91373baea21948348c1d5b9e4433257de5
Vivek Kumar <vivek142000kumar@gamil.com>
2025-12-25 23:27:14 +0530

    robot

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,19 +1,11 @@
-export function GET() {
-  const content = `
-User-agent: *
-Allow: /
-
-# Allow essential assets
-Allow: /_next/static/
-Allow: /_next/image/
-
-# Sitemap
-Sitemap: https://sarkariphototool.online/sitemap.xml
-  `.trim();
-
-  return new Response(content, {
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://sarkariphototool.online/sitemap.xml",
+  };
 }
